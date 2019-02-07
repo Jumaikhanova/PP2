6508a1bd5c7990dcfab9c64da83f098731eb9dbb
Jumaikhanova <47049824+Jumaikhanova@users.noreply.github.com>
2019-02-07 13:01:38 +0600

    Update Program.cs
    Task3 Week1 is changed, due to a misunderstanding of the task.

--- Week1/Task3/Task3/Program.cs
+++ Week1/Task3/Task3/Program.cs
@@ -4,21 +4,25 @@ namespace Task2
 {
     class Program
     {
-        static void Double(int k) //a method that repeats each element of a given array
-        {
-            Console.Write(k + " " + k + " ");
-        }
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine()); //conversion to integer
             int[] a = new int[n]; //creating the array
+            int[] b = new int[n * 2]; //creating the array
             string[] arr = Console.ReadLine().Split(); //splitting the input string
-            foreach (string k in arr) //moving through the array arr
+            for (int i = 0; i < a.Length; i++)
             {
-                Double(int.Parse(k)); //method call
+                a[i] = int.Parse(arr[i]); //conversion to integer
+                for (int j=i*2; j<=2*i+1; j++) 
+                {
+                    b[j] = a[i];//assigning elements to a new array
+                }
             }
-            
-        }
-        
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.Write(b[i]+ " ");
+            }
+
+        }  
     }
 }
